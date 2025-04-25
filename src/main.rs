fd5067eee7613f9ef0f827097b9aa797ea6cee00
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-04-25 15:39:34 -0500

    LMAO

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
 use {
-    crate::cli::vidtoss,
     clap::Parser,
     cli::CliArgs,
-    image::{AnimationDecoder, RgbaImage, codecs::gif::GifDecoder, imageops},
-    std::{error::Error, fs::File, io::BufReader, path::Path, process::Stdio},
-=======
-use clap::Parser;
-use cli::CliArgs;
-use image::{AnimationDecoder, DynamicImage, Frame, RgbaImage, codecs::gif::GifDecoder, imageops};
-use rand::Rng;
-use std::{
-    fs::File,
-    io::BufReader,
-    path::{Path, PathBuf},
->>>>>>> 540b0ad (fixed it lol)
+    cli::vidtoss,
+    image::{AnimationDecoder, DynamicImage, Frame, RgbaImage, codecs::gif::GifDecoder, imageops},
+    rand::Rng,
+    std::{
+        error::Error,
+        fs::File,
+        io::BufReader,
+        path::{Path, PathBuf},
+        process::Stdio,
+    },
 };
 
 mod cli;
@@ -35,12 +31,8 @@ fn convert_vid_to_gif_file(
     input_vid_path: &Path,
     output_gif_path: &Path,
     fps: u32,
-<<<<<<< HEAD
     scale: Vec<i64>,
 ) -> anyhow::Result<(), Box<dyn Error>> {
-=======
-) -> anyhow::Result<(), Box<dyn std::error::Error>> {
->>>>>>> 540b0ad (fixed it lol)
     let status = std::process::Command::new("ffmpeg")
         .arg("-i")
         .arg(input_vid_path)
@@ -50,16 +42,8 @@ fn convert_vid_to_gif_file(
             fps, scale[0], scale[1]
         ))
         .arg(output_gif_path)
-<<<<<<< HEAD
         .stdout(Stdio::null())
         .output()?;
-=======
-        .status()?;
-
-    if !status.success() {
-        return Err("Failed to convert video to GIF".into());
-    }
->>>>>>> 540b0ad (fixed it lol)
 
     if !status.status.success() {
         return Err("Failed to convert video to GIF".into());
@@ -82,12 +66,8 @@ fn create_sprite_sheet(
     let decoder = GifDecoder::new(reader)?;
     let frames = decoder.into_frames().collect_frames()?;
     let num_frames = frames.len() as u32;
-<<<<<<< HEAD
     let mut frame_count: u32 = 0;
     let rows = num_frames.div_ceil(columns);
-=======
-    let rows = (num_frames + columns - 1) / columns;
->>>>>>> 540b0ad (fixed it lol)
     let (tile_width, tile_height) = tile_size;
 
     let mut ss = RgbaImage::new(tile_width * columns, tile_height * rows);
@@ -125,7 +105,6 @@ fn create_sprite_sheet(
 
     ss.save(output_png)?;
 
-<<<<<<< HEAD
     let (lfx, lfy) = last_frame_pos(columns, rows, frame_count);
 
     Ok((rows, columns, lfx, lfy, frame_count))
@@ -133,56 +112,4 @@ fn create_sprite_sheet(
 
 fn main() -> anyhow::Result<(), Box<dyn Error>> {
     vidtoss()
-=======
-    let last_frame_x = (columns - 1) * tile_width;
-    let last_frame_y = (rows - 1) * tile_height;
-
-    Ok((rows, columns, last_frame_x, last_frame_y))
-}
-
-fn main() -> anyhow::Result<(), Box<dyn std::error::Error>> {
-    let args = CliArgs::parse();
-
-    let tile_size = if args.tile_size.len() == 2 {
-        (args.tile_size[0], args.tile_size[1])
-    } else {
-        (64, 64)
-    };
-
-    let rand_id = rand::rng().random::<u64>();
-
-    convert_vid_to_gif_file(
-        &args.vid_in,
-        Path::new(&format!("temp_gif_out-{}.gif", rand_id)),
-        args.fps.unwrap_or(10),
-    )?;
-
-    let output_png = args.vid_in.with_extension("spritesheet.png");
-    let (rows, columns, last_frame_x, last_frame_y) = create_spritesheet(
-        Path::new(&format!("temp_gif_out-{}.gif", rand_id)),
-        tile_size,
-        args.columns.unwrap_or(30),
-        &output_png,
-    )
-    .map_err(|e| {
-        println!("{}", e);
-    })
-    .expect("Fuck");
-
-    println!("Completed spritesheet generation!");
-
-    if args.info.unwrap() {
-        println!(
-            "Dimensions:\n - Rows ====> {}\n - Columns => {}",
-            rows, columns
-        );
-
-        println!(
-            "Last frame is located at:\n- X => {}\n- Y => {}",
-            last_frame_x, last_frame_y
-        );
-    }
-
-    Ok(())
->>>>>>> 540b0ad (fixed it lol)
 }
